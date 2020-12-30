a22760ebb6797d519cf99d8e3ea4872384f4c95c
andreacolina <a.colina.2020@alumnos.urjc.es>
2020-12-30 13:27:32 +0100

    Server: added endpoint GET eolicplant by id

--- server/src/routes/plant.js
+++ server/src/routes/plant.js
@@ -8,11 +8,13 @@ module.exports = (app, Plant, queue) => {
         Plant.create({
             city: req.body.city
         }).then((result) => {
-            res.json({ id: result.id,
+            res.json({
+                id: result.id,
                 city: result.city,
                 progress: 0,
                 completed: false,
-                planning: null });
+                planning: null
+            });
             queue.sendMessage(JSON.stringify({id: result.id, city: result.city}));
         }).catch(function (err) {
             console.log(err);
@@ -28,4 +30,17 @@ module.exports = (app, Plant, queue) => {
             }
         }).then((result) => res.json(result))
     );
+
+    app.get("/eolicplants/:id", (req, res) =>
+        Plant.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (result) {
+            if (!result) {
+                res.status(404).send("Not found eolicplant at id:" + req.params.id);
+            } else {
+                res.json(result).status(200);
+            }
+        }));
 }
